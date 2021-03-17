b7074abc6650b44228efcbcc8afecfc2c384414e
PierreMonrocq <pierre.monrocq@outlook.fr>
2021-03-17 11:51:50 +0100

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# SIA-MASTER1
-Projet de SIA M1 MIAGE Paris Dauphine
+# Wordpress plugin for an humanitarian organization
+
+plugins used with this site:
+- akismet
+- bbpress
+- loginizer
+- members
+- moderation-tools-for-bbpress
--- index.php
+++ index.php
@@ -1,2 +1,31 @@
-<?php
-// Silence is golden.
+<?php get_header();?>
+
+<div class="breadcrumb-info">
+<div class="pi-inner" style="background-image: url(' <?php echo get_template_directory_uri() ?>/img/africa.jpg');">>
+		<div class="container">
+			<div class="bread">
+			<h1><?php wp_title(''); ?></h1>
+			<div class="site-breadcrumb">
+				<a href="<?= home_url() ?>">Accueil</a> <i class="fa fa-angle-right"></i>
+				<span><?php wp_title(''); ?></span>
+			</div>
+			</div>
+		</div>
+	</div>
+</div>
+    	<main id="main" class="site-content">
+		<div class="article-section">
+		<div class="container">
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+					<div class="entry">
+						<?php the_content(); ?>
+					</div><!-- entry -->
+			<?php endwhile; ?>
+			<?php endif; ?>
+		</div>
+		
+		</div>
+    	</main>
+
+<?php get_footer() ?>
+
